bc066f93d31fe722dad3964bbf8933fb7b8e46f6
highjeans <highjeans11@hotmail.com>
2022-07-16 12:36:51 -0700

    Update sendannouncement to only allow professors to run the command

--- commands/sendannouncement.js
+++ commands/sendannouncement.js
@@ -9,6 +9,11 @@ module.exports = {
                 .setDescription('The announcement to send everyone.')
                 .setRequired(true)),
     async execute(interaction) {
+        if (!interaction.member.roles.cache.some(role => role.name === 'Professor')) {
+            await interaction.reply({content: "Only Professors can send announcements.", ephemeral: true});
+            return;
+        }
+
         const studentRole = await interaction.guild.roles.cache.find(role => role.name === "Student");
         await interaction.channel.send(`<@&${studentRole.id}> ` + interaction.options.getString("announcement"));
         await interaction.reply({content: "Announcement Sent!", ephemeral: true});
